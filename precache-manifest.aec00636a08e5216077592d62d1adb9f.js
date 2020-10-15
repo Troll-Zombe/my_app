33f6f04f3671ceccb070d85744cfaa06455d922b
uqir99 <RS.redstone7@gmail.com>
2020-10-15 15:24:39 +0900

    Updates

---
+++ precache-manifest.aec00636a08e5216077592d62d1adb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad5e65b85d66fbba694ced91591a11a",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "ada64b529f2c03112a3b",
+    "url": "/my_app/static/css/main.84cfd165.chunk.css"
+  },
+  {
+    "revision": "82701ddb4f48895dcb54",
+    "url": "/my_app/static/js/2.7966d3d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_app/static/js/2.7966d3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada64b529f2c03112a3b",
+    "url": "/my_app/static/js/main.4ec75fdb.chunk.js"
+  },
+  {
+    "revision": "209157daf82b8d21beaf",
+    "url": "/my_app/static/js/runtime-main.dd845438.js"
+  }
+]);
